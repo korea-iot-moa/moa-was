758d3f151a6091b27ac012d903d0e8323adc7e1b
Youngjun9909 <dydekd7511@gmail.com>
2024-11-06 15:05:00 +0900

    was

---
+++ moa/src/main/java/com/korit/moa/moa/common/constant/ApiMappingPattern.java
@@ -0,0 +1,40 @@
+package com.korit.moa.moa.common.constant;
+
+public class ApiMappingPattern {
+    // 인증 절차
+    // : 회원가입, 로그인
+    public static final String AUTH = "";
+
+    // 회원 관련
+    public static final String USER = "";
+
+    // 모임 관련
+    public static final String GROUP = "";
+
+    // 추천 관련
+    public static final String RECOMMENDATION = "";
+
+    // 유저 리스트(모임 내 유저)
+    public static final String USER_LIST = "";
+
+    // 모임 참여 요청 관련 로직
+    public static final String USER_ANSWER = "";
+
+    // 후기 관련
+    public static final String REVIEW = "";
+
+    // 투표 관련
+    public static final String VOTE = "";
+
+    // 투표 답변 관련
+    public static final String VOTE_RESULT = "";
+
+    // 신고 관련
+    public static final String REPORT = "";
+
+    // 블랙 리스트
+    public static final String BLACK_LIST = "";
+
+    // 게시판 관련
+    public static final String NOTICE = "";
+}
