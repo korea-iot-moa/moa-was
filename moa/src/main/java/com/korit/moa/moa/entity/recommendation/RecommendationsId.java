b05786349178ded03a5e82269bdb7f72293bd1cf
Youngjun9909 <dydekd7511@gmail.com>
2024-11-12 09:05:19 +0900

    was

---
+++ moa/src/main/java/com/korit/moa/moa/entity/recommendation/RecommendationsId.java
@@ -0,0 +1,17 @@
+package com.korit.moa.moa.entity.recommendation;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class RecommendationsId implements Serializable {
+    private Long groupId;
+    private String userId;
+}
