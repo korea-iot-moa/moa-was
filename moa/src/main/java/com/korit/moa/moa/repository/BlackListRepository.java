37e492b50d526ae0fafbc92947541bbc7e207db3
als5855 <wlals5855@naver.com>
2024-11-14 12:34:04 +0900

    20241114 jjm2

--- moa/src/main/java/com/korit/moa/moa/repository/BlackListRepository.java
+++ moa/src/main/java/com/korit/moa/moa/repository/BlackListRepository.java
@@ -1,9 +1,10 @@
 package com.korit.moa.moa.repository;
 
+import com.korit.moa.moa.entity.balckList.BlackList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlackListRepository extends JpaRepository {
+public interface BlackListRepository extends JpaRepository<BlackList, Long> {
     
 }
--- moa/src/main/java/com/korit/moa/moa/repository/NoticeRepository.java
+++ moa/src/main/java/com/korit/moa/moa/repository/NoticeRepository.java
@@ -1,9 +1,10 @@
 package com.korit.moa.moa.repository;
 
+import com.korit.moa.moa.entity.notice.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NoticeRepository extends JpaRepository {
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
 }
