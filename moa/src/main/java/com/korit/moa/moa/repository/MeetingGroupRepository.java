5a64372580c5268af03dfd33eecae82668c82be1
als5855 <wlals5855@naver.com>
2024-11-14 18:01:32 +0900

    20241114 jjm5

--- moa/src/main/java/com/korit/moa/moa/repository/MeetingGroupRepository.java
+++ moa/src/main/java/com/korit/moa/moa/repository/MeetingGroupRepository.java
@@ -2,8 +2,14 @@ package com.korit.moa.moa.repository;
 
 import com.korit.moa.moa.entity.meetingGroup.MeetingGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MeetingGroupRepository extends JpaRepository<MeetingGroup,Long> {
+
+ 
 }
