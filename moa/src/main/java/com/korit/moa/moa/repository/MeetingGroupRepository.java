05d3125e96edf07be12110013d5377a76187f0f0
als5855 <wlals5855@naver.com>
2024-11-18 17:04:55 +0900

    20241118 jjm

--- moa/src/main/java/com/korit/moa/moa/repository/MeetingGroupRepository.java
+++ moa/src/main/java/com/korit/moa/moa/repository/MeetingGroupRepository.java
@@ -27,24 +27,24 @@ public interface MeetingGroupRepository extends JpaRepository<MeetingGroup,Long>
 
 
     // 그룹 모임 홈화면 출력 사용자가 카테고리 선택한 경우
-    @Query("SELECT groupId, group_title, groupAddress, meetingType, groupImage, groupDate" +
-            "FROM (SELECT mg.groupdId, u.userId, mg.groupTitle, mg.groupAddress, mg.groupCategory, mg.groupImage, mg.groupDate" +
-            "ROW_NUMBER OVER (PARTITION BY mg.group_category ORDER BY group_title) AS rn" +
-            "FROM MeetingGroup mg" +
-            "INNER JOIN Users u ON mg.group_category = u.hobby" +
-            "WHERE u.user_id = 'userId'" +
+    @Query(value = "SELECT groupId, groupTitle, groupAddress, meetingType, groupImage, groupDate " +
+            "FROM (SELECT mg.groupdId, u.userId, mg.groupTitle, mg.groupAddress, mg.groupCategory, mg.groupImage, mg.groupDate " +
+            "ROW_NUMBER OVER (PARTITION BY mg.group_category ORDER BY group_title) AS rn " +
+            "FROM MeetingGroup mg " +
+            "INNER JOIN Users u ON mg.group_category = u.hobby " +
+            "WHERE u.user_id = 'userId' " +
             "AND mg.meetingType IN ('취미', '문화_예술', '스포츠_운동', '푸드_맛집', '자기계발', '여행', '연애', '힐링') ) AS ranked " +
             "WHERE ranked.rn <= 3" +
-            "ORDER BY group_category, RAND()")
+            "ORDER BY group_category, RAND() ")
     List<Object[]> findGroupHomeSelectByGroupId(@Param("groupId") Long goupId);
 
     // 그룹 모임 홈화면 출력 사용자가 카테고리 선택한 경우
-    @Query("SELECT groupId, groupTitle, groupAddress, groupCategory, groupImage, groupDate" +
-            "FROM (SELECT groupId, groupTitle, groupAddress, groupCategory, groupImage, groupDate" +
-            "ROW_NUMBER() OVER (PARTITION BY mg.group_category ORDER BY group_title) AS rn" +
+    @Query(value = "SELECT groupId, groupTitle, groupAddress, groupCategory, groupImage, groupDate " +
+            "FROM (SELECT groupId, groupTitle, groupAddress, groupCategory, groupImage, groupDate " +
+            "ROW_NUMBER() OVER (PARTITION BY mg.group_category ORDER BY group_title) AS rn " +
             "FROM MeetingGroup" +
-            "WHERE mg.groupCategory IN ('취미', '문화_예술', '스포츠_운동', '푸드_맛집', '자기계발', '여행', '연애', '힐링')) AS ranked" +
-            "WHERE ranked.rn <= 3" +
+            "WHERE mg.groupCategory IN ('취미', '문화_예술', '스포츠_운동', '푸드_맛집', '자기계발', '여행', '연애', '힐링')) AS ranked " +
+            "WHERE ranked.rn <= 3 " +
             "ORDER BY grouptCategory, RAND()")
     List<Object[]> findGroupHomeNoSelectByGroupId(@Param("groupId") Long goupId);
 }
