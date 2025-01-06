416bd37a383508c9313f0a259c4453d440d808bd
devgiants75 <devgiants75@naver.com>
2025-01-06 17:15:06 +0900

    20240106 websocket connection success

--- moa/src/main/java/com/korit/moa/moa/repository/ChatRepository.java
+++ moa/src/main/java/com/korit/moa/moa/repository/ChatRepository.java
@@ -2,10 +2,13 @@ package com.korit.moa.moa.repository;
 
 import com.korit.moa.moa.entity.chat.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findByRoomIdOrderByTimestampAsc(Long roomId);
+    @Query("SELECT cm FROM ChatMessage cm WHERE cm.chatRoom.chatRoomId = :roomId ORDER BY cm.createdAt ASC")
+    List<ChatMessage> findByRoomIdOrderByTimestampAsc(@Param("roomId") Long roomId);
 }
 
